ef45fd3209bdb55cc2325d891c165750612c0c03
Felicia Oentoro <felicia.oentoro@gmail.com>
2015-01-09 22:30:51 -0600

    edited OI, HW, Robot, and added drivetrain subsystem, arcade drive command, and tank drive command

--- src/org/discobots/recyclerush/HW.java
+++ src/org/discobots/recyclerush/HW.java
@@ -7,7 +7,13 @@ package org.discobots.recyclerush;
  */
 public class HW {
     /* Motors */
-	
+	public static int motorPwm0 = 0;
+	public static int motorPwm1 = 1;
+	public static int motorPwm2 = 2;
+	public static int motorPwm3 = 3;
+	public static int motorPwm4 = 4;
+	public static int motorPwm5 = 5;
+	public static int motorPwm6 = 6;
 	
 	/* Solenoids */ 
 	
--- src/org/discobots/recyclerush/Robot.java
+++ src/org/discobots/recyclerush/Robot.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 import org.discobots.recyclerush.commands.AutonomousCommand;
+import org.discobots.recyclerush.subsystems.DrivetrainSubsystem;
 import org.discobots.recyclerush.subsystems.PowerInfoSubsystem;
 import org.discobots.recyclerush.utils.Dashboard;
 
@@ -21,6 +22,7 @@ public class Robot extends IterativeRobot {
  
 	public static PowerInfoSubsystem powerInfoSub;
 	public static OI oi;
+	public static DrivetrainSubsystem drivetrainSubsystem;
 
     AutonomousCommand autonomousCommand;
 
@@ -32,6 +34,7 @@ public class Robot extends IterativeRobot {
 		// global oi & subsystem code
     	oi = new OI();
 		powerInfoSub = new PowerInfoSubsystem();
+		drivetrainSubsystem = new DrivetrainSubsystem();
 		
 		// autonomous command
         autonomousCommand = new AutonomousCommand();
