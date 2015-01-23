b0893c59728cdd657111a26755454e664d6a36ce
Mason <mason2smart@gmail.com>
2015-01-23 17:52:36 -0600

    Updated Lift Command

--- src/org/discobots/recyclerush/commands/LiftCommand.java
+++ src/org/discobots/recyclerush/commands/LiftCommand.java
@@ -8,29 +8,31 @@ import edu.wpi.first.wpilibj.command.Command;
  *
  */
 public class LiftCommand extends Command {
-
-    public LiftCommand() {
+	float liftSPD;
+    public LiftCommand(float liftSPD) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.liftSub);
+    	this.liftSPD=liftSPD;//sets speed
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.liftSub.setLiftSpeed(liftSPD);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.liftSub.setLiftSpeed(0);
     }
 
     // Called when another command which requires one or more of the same
