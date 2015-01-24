2d5209208d3fe275152bb5ed2a606dc4bb9fca0e
Mason <mason2smart@gmail.com>
2015-01-24 16:19:58 -0600

    Updated Lift Subsystem and Finished Intake Command

--- src/org/discobots/recyclerush/subsystems/IntakeSubsystem.java
+++ src/org/discobots/recyclerush/subsystems/IntakeSubsystem.java
@@ -1,6 +1,7 @@
 package org.discobots.recyclerush.subsystems;
 
 import org.discobots.recyclerush.HW;
+import org.discobots.recyclerush.commands.IntakeCommand;
 import org.discobots.recyclerush.commands.drive.TankDriveCommand;
 
 import edu.wpi.first.wpilibj.CANTalon;
@@ -10,25 +11,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class IntakeSubsystem extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-	CANTalon intakeLeft, intakeRight;
-	
-	
-	public IntakeSubsystem()
-	{
-		
-		intakeLeft=new CANTalon(HW.intakeLeft);
-		intakeRight=new CANTalon(HW.intakeRight);
-	}
-	public void setIntakeSpeed(double intakeSPD)
-	{
-	intakeLeft.set(intakeSPD);
-	intakeLeft.set(intakeSPD);
-	}
-    public void initDefaultCommand() {
-    	setDefaultCommand(new IntakeCommand());
-    }
-}
 
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	CANTalon intakeLeft, intakeRight;
+
+	public IntakeSubsystem() {
+
+		intakeLeft = new CANTalon(HW.intakeLeft);
+		intakeRight = new CANTalon(HW.intakeRight);
+	}
+
+	public void setIntakeSpeed(double intakeSPD) {
+		intakeLeft.set(intakeSPD);
+		intakeLeft.set(intakeSPD);
+	}
+
+	public void initDefaultCommand() {
+	}
+}
--- src/org/discobots/recyclerush/subsystems/LiftSubsystem.java
+++ src/org/discobots/recyclerush/subsystems/LiftSubsystem.java
@@ -11,28 +11,21 @@ import org.discobots.recyclerush.commands.drive.TankDriveCommand;
  */
 public class LiftSubsystem extends Subsystem {
 
-	
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 	CANTalon liftMotor1, liftMotor2;
 
-	public LiftSubsystem()
-	{
+	public LiftSubsystem() {
 		liftMotor1 = new CANTalon(HW.motorLift1);
 		liftMotor2 = new CANTalon(HW.motorLift2);
 	}
-	
-	public void setLiftSpeed(double liftSPD)
-	{
+
+	public void setLiftSpeed(double liftSPD) {
 		liftMotor1.set(liftSPD);
 		liftMotor2.set(liftSPD);
 	}
-    
-	public void initDefaultCommand() {
-		setDefaultCommand(new TankDriveCommand());
-	}
-	
-	
-}
 
+	public void initDefaultCommand() {
+	}
+
+}
