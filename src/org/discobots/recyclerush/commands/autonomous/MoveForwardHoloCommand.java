79bc28b6f41bce681ace620cc8f85a01710b1b52
Nolan Shah <nolanshah212@gmail.com>
2015-03-07 18:37:46 -0600

    Small autonomous changes and autonomous commands now have pid.

--- src/org/discobots/recyclerush/commands/autonomous/MoveForwardHoloCommand.java
+++ src/org/discobots/recyclerush/commands/autonomous/MoveForwardHoloCommand.java
@@ -1,5 +1,8 @@
 package org.discobots.recyclerush.commands.autonomous;
 
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import org.discobots.recyclerush.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
@@ -13,22 +16,42 @@ public class MoveForwardHoloCommand extends Command {
 	long duration;
 	double speed;
 	
+	PIDController controller;
+	PIDSource controllerSource;
+	PIDOutput controllerOutput;
+	double controllerSourceVal;
+	double controllerOutputVal;
+	
     public MoveForwardHoloCommand(long time, double speed) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.driveTrainSub);
     	duration = time;
     	this.speed = speed;
+    	controllerSource = new PIDSource() {
+			public double pidGet() {
+				return controllerSourceVal;
+			}
+    	};
+    	controllerOutput = new PIDOutput() {
+			public void pidWrite(double output) {
+				controllerOutputVal = output;
+			}
+    	};
+    	controller = new PIDController(1.0/30.0, 0, 0, controllerSource, controllerOutput);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	time = System.currentTimeMillis();
+    	controller.setSetpoint(Robot.driveTrainSub.getAngle());
+    	controller.enable();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrainSub.holonomicDriveRamp(-speed, 0, 0);
+    	this.controllerSourceVal = Robot.driveTrainSub.getAngle();
+    	Robot.driveTrainSub.holonomicDriveRamp(-speed,0, controllerOutputVal);
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -39,7 +62,8 @@ public class MoveForwardHoloCommand extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrainSub.holonomicDriveRamp(0, 0, 0);
+    	controller.disable();
+    	Robot.driveTrainSub.holonomicDriveUnramped(0, 0, 0);
     }
 
     // Called when another command which requires one or more of the same
